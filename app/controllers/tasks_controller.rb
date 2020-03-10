0e0322ed693996023d509d5e5a0e990b6d5f5d37
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-10 16:32:06 +0100

    The exercise is done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+# require "pry-byebug"
+
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    # binding.pry
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    # raise
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    parameters = params.require(:task).permit(:title, :details, :completed)
+    # parameters[:completed] = parameters[:completed] == "1"
+    # return parameters
+  end
+end
---
+++ app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ApplicationRecord
+  # validates :title, :details, presence: true
+  # :completed default to false
+end
